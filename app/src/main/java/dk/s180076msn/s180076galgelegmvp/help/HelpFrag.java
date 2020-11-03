426891486c66a6691b4b92dfcccd701dd45299d6
Mads Storgaard-Nielsen <s180076@win.dtu.dk>
2020-11-03 17:05:28 +0100

    Done

--- app/src/main/java/dk/s180076msn/s180076galgelegmvp/help/HelpFrag.java
+++ app/src/main/java/dk/s180076msn/s180076galgelegmvp/help/HelpFrag.java
@@ -7,16 +7,36 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import dk.s180076msn.s180076galgelegmvp.MainMenuFrag;
 import dk.s180076msn.s180076galgelegmvp.R;
 
 
-public class HelpFrag extends Fragment {
+public class HelpFrag extends Fragment implements View.OnClickListener {
+    Button goToMenu;
+    Fragment f;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_help, container, false);
+        goToMenu = root.findViewById(R.id.gotoMainMenu);
 
+        goToMenu.setOnClickListener(this);
         return root;
+    }
+
+    public void setFragment(Fragment f) {
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.MainActivityFL, f)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        f = new MainMenuFrag();
+        setFragment(f);
     }
 }
